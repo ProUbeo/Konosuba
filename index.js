d8b64ac774ae7d1cfc69d0bf77641ca702a1a75f
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-06-20 18:17:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ db.defaults({ histoires: [], xp: []}).write()
 
  var prefix = ("k!")
  var randnum = 0;
- var teste = false;
  
  bot.on('ready', function() {
      bot.user.setUsername("Konosuba")
@@ -189,26 +188,7 @@ if(teste == true){
     teste = false;
 }else
 message.channel.send(`aucun teste en cours`);
-}
-
-if(message.content === "k!teste"){
-    if("234368202379886593 323807479651631104".includes(message.author.id)){
-        if(teste == true){ 
-            message.channel.send(`**${message.author.username}** fait apparaître un teste !`,{
-            file:"https://ih1.redbubble.net/image.14778725.5688/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u3.jpg"
-        })}
-        if(teste == false){ 
-            message.channel.send(`**${message.author.username}** a oublier un truc !`,{
-                file:"https://i.paigeeworld.com/user-media/1477785600000/577051e1777e99162683eb41_5815402246705b5d2a0e122c_320.jpg"
-            })}
-}else{
-            message.channel.send(`tu n'est pas le créateur`,{
-                file:"http://1.bp.blogspot.com/-45wb-emSlEM/TggFAb-44hI/AAAAAAAAAhg/DaXtkwQg6O8/s1600/freak+out.jpg"
-            })}
-    }
-
-
-    
+}    
 if(message.content.startsWith("k!arc")){
     message.channel.send(`**${message.author.username}** fait apparaître son arc !`,{
     file:"http://img4.hostingpics.net/pics/484588arc.png"
